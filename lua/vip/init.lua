bd54f78b4e5384f381bfe754a49db53d2bee83b1
Moritz Pollack <werdaslesenkannistschlau@gmail.com>
2022-09-17 18:14:49 +0200

    Maybe thi time

--- lua/vip/init.lua
+++ lua/vip/init.lua
@@ -3,13 +3,13 @@ local M = {}
 M._opt = ""
 
 M.setup = function(opt)
-    M._opt = opt   
+    M._opt = opt
 
     local group = vim.api.nvim_create_augroup("vip_save", { clear = true })
     vim.api.nvim_create_autocmd(
         "BufWrite",
         {
-            command = print(M._opt),
+            command = "echo '" + M._opt + "'",
             group = group,
         }
     )
