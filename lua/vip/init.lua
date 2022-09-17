4d74fb63903baac27e921f927f600b190df8723d
Moritz Pollack <werdaslesenkannistschlau@gmail.com>
2022-09-17 18:40:55 +0200

    sould  work now

--- lua/vip/init.lua
+++ lua/vip/init.lua
@@ -1,19 +1,14 @@
 local M = {}
 
-M._opt = ""
 
 M.setup = function(opt)
-    M._opt = opt
-
-    local cmd = "echo '"..M._opt.."'"
-    print(opt)
-    print("🤣")
-
     local group = vim.api.nvim_create_augroup("vip_save", { clear = true })
     vim.api.nvim_create_autocmd(
         "BufWrite",
         {
-            command = cmd,
+            callback = function()
+                print(opt)
+            end,
             group = group,
         }
     )
