11b628dd320b0b2c9b97963e5a6f93932a10270e
Moritz Pollack <werdaslesenkannistschlau@gmail.com>
2022-09-17 18:10:22 +0200

    anoter error

--- lua/vip/init.lua
+++ lua/vip/init.lua
@@ -5,20 +5,14 @@ M._opt = ""
 M.setup = function(opt)
     M._opt = opt   
 
-    local group = vim.api.nvim_create_augroup("vip_save")
+    local group = vim.api.nvim_create_augroup("vip_save", { clear = true })
     vim.api.nvim_create_autocmd(
         "BufWrite",
         {
             command = print(M._opt),
+            group = group,
         }
     )
-
-    vim.api.nvim_command([[
-        augroup PrintOnSave
-            autocmd!
-            autocmd BufWrite * lua print(require('vip')._opt)
-        augroup END
-    ]])
 end
 
 return M
