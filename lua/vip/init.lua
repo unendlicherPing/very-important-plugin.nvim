16389f5544db21664cdd611f6cd2de8ae0ccd140
Moritz Pollack <werdaslesenkannistschlau@gmail.com>
2022-09-17 18:08:29 +0200

    i hope I fixed the bug

--- lua/vip/init.lua
+++ lua/vip/init.lua
@@ -5,10 +5,18 @@ M._opt = ""
 M.setup = function(opt)
     M._opt = opt   
 
+    local group = vim.api.nvim_create_augroup("vip_save")
+    vim.api.nvim_create_autocmd(
+        "BufWrite",
+        {
+            command = print(M._opt),
+        }
+    )
+
     vim.api.nvim_command([[
         augroup PrintOnSave
             autocmd!
-            autocmd BufWrite * :lua print(require('vip')._opt)
+            autocmd BufWrite * lua print(require('vip')._opt)
         augroup END
     ]])
 end
