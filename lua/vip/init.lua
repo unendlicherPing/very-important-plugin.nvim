16489acd564ae701fd9e1559849c159a1749eebf
Moritz Pollack <werdaslesenkannistschlau@gmail.com>
2022-09-18 00:25:53 +0200

    aljasm

--- lua/vip/init.lua
+++ lua/vip/init.lua
@@ -4,7 +4,7 @@ local M = {}
 M.setup = function(opt)
     local group = vim.api.nvim_create_augroup("vip_save", { clear = true })
     vim.api.nvim_create_autocmd(
-        "BufWrite",
+        "BufWritePost",
         {
             callback = function()
                 print("something else")
